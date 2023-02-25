fda754d4f90c2aed4f4e4763ff5e225a0e957569
LuxScintilla <catherina.ea@gmail.com>
2023-02-25 19:26:59 +0000

    Expand with homepage and cleanup theme

--- javascript.js
+++ javascript.js
@@ -1,25 +1,7 @@
-var cards = document.querySelectorAll('.card');
+let cards = document.querySelectorAll('.card');
 
 [...cards].forEach((card)=>{
   card.addEventListener( 'click', function() {
     card.classList.toggle('is-flipped');
   });
 });
-
-
-// function highLight(text) {
-//     let input = document.getElementById('search-text').value;
-//     input = input.toLowerCase();
-//     let innerHTML = input.innerHTML;
-//     let index = innerHTML.indexOf(text)
-//     if (index >= 0) {
-//         innerHTML = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
-//         input.innerHTML = innerHTML;
-//     }
-// }
-
-
-
-// function search(string){
-//     window.find(string);
-// }
